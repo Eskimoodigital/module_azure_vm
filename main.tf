5073869233aa62eb5ad8787bd736cee93ce2bc37
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-11-04 09:30:27 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,56 @@
+resource "azurerm_resource_group" "example" {
+  name     = "RGEskTfm"
+  location = "West Europe"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = module.spoke_azure_1[0].vnet.subnets[0].subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+
+
+  }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "EskimooPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  allocation_method = "Dynamic"
+
+}
+
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "EskimooTest"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  size                            = "Standard_F2"
+  disable_password_authentication = false
+  admin_username                  = "adminuser"
+  admin_password                  = "Password123!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
